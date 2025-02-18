3fc2ee22168aa276682da067d323d89888235263
carlosjeferson <carlosjjs500214@gmail.com>
2025-02-18 13:17:37 -0300

    atualizações strapi

---
+++ backend/src/api/instrutor/routes/instrutor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * instrutor router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::instrutor.instrutor');
---
+++ backend/src/api/instrutor/services/instrutor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * instrutor service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::instrutor.instrutor');
